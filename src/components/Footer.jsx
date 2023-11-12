dd42b43394af076015878b6056a4fc0f08eeb8e3
Dhananjay Shahane <143813959+DhananjayShahane@users.noreply.github.com>
2023-11-12 12:56:55 +0530

    update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,7 +8,7 @@ function Footer (){
                     </p>
                 </div>
                 <div className="footer_footerImageWrapper text-center flex justify-center">
-                   <img alt="footer-img" width="900" srcSet="../src/assets/img/footer-bg.png" src="../src/assets/img/footer-bg.png"></img>
+                   <img alt="footer-img" width="900" srcSet="./img/footer-bg.png" src="./img/footer-bg.png"></img>
                 </div>
             </footer>
         </>
